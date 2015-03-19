512e86c69a0b3cd7035268ef344e7a2a565a7e40
J-Salaheddine <alsalah6@hotmail.com>
2015-03-19 13:26:43 +0100

    c6de

--- src/com/projet/image/ImageWindows.java
+++ src/com/projet/image/ImageWindows.java
@@ -49,7 +49,11 @@ public class ImageWindows extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private String pathImage;
+	int[] tabR = new int[256] ;
+	int[] tabG = new int[256] ;
+	int[] tabB = new int[256] ;
+	
+	int[] HisCumuler = new int[256] ;
 	
 	public ImageWindows(String pathImage) {
 		BufferedImage image = null;
@@ -59,62 +63,73 @@ public class ImageWindows extends JFrame {
 		catch (IOException e) {
 		 e.printStackTrace();
 		}
-		int[] tabR = new int[256] ;
-		int[] tabG = new int[256] ;
-		int[] tabB = new int[256] ;
-		
-		int[] HisCumuler = new int[256] ;
-		
-		int w = image.getWidth();
-		int h = image.getHeight();
-		for(int i=0; i<w; i++){
-		 for(int j=0; j<h; j++){
-		  /* Accéder au rgb pixel (i, j) */
-		  int rgb = image.getRGB(i,j);
-		    int r = (rgb >> 16) & 0xff;
-		    int g = (rgb >> 8) & 0xff;
-		    int b = rgb & 0xff;
 
-		    tabR[r]++;
-		    tabG[g]++;
-		    tabG[b]++;
-		 }
+		
+		remplirTableRGB(image);
+		calculeHistogrammeCumuler();
+		affiche();
+		
+		/*
+		 * Calcule des valuers: rg, by et wb
+		 * on utilise la longeure du tableau R (tout les tableau ont la même longeure
+		 */
+		int rg, by,wb;
+		for(int i=0; i<tabR.length ;i++){
+			
 		}
 		
+
+		
+	     
+		initUI(image);
+	}
+
+	/**
+	 * Récupérer les valeurs rgb de chaque pixele de l'image
+	 * stocker les valeurs dans 3 tableaux differants
+	 * @param image
+	 */
+	public void remplirTableRGB(BufferedImage image) {
+		int w = image.getWidth();
+		int h = image.getHeight();
+		for (int i = 0; i < w; i++) {
+			for (int j = 0; j < h; j++) {
+				/* Accéder au rgb pixel (i, j) */
+				int rgb = image.getRGB(i, j);
+				int r = (rgb >> 16) & 0xff;
+				int g = (rgb >> 8) & 0xff;
+				int b = rgb & 0xff;
+
+				tabR[r]++;
+				tabG[g]++;
+				tabG[b]++;
+			}
+		}
+	}
+	
+	/**
+	 * Calcule de l'histogramme cumuler
+	 */
+	public void calculeHistogrammeCumuler(){
 		//rermplissage de l'histogramme cumuler
 		//valeur i des trois tableau stocker dans le 4eme
 		for(int i=0; i<tabR.length ;i++){
 			HisCumuler[i] = tabR[i] + tabG[i] + tabB[i];
 		}
-		
-		
+	}
+	
+	public void affiche(){
 		for(int i=0; i<tabR.length ; i++){
 			System.out.print("R: "+i+" :"+tabR[i]);
 			System.out.print("   G:"+i+" :"+tabG[i]);
 			System.out.print("   B "+i+" :"+tabB[i]);
 			System.out.println("       Cumulée: "+HisCumuler[i]);
 		}
-		
-	    // Transformer le tableau en liste
-	    List listeR = Arrays.asList(tabR);
-	    List listeG = Arrays.asList(tabG);
-	    List listeB = Arrays.asList(tabB);
-	 
-	    // Utiliser la liste comme le paramètre du vecteur
-	    Vector vecR = new Vector(listeR);   // La conversion se fait ici
-	    Vector vecG = new Vector(listeG);   // La conversion se fait ici
-	    Vector vecB = new Vector(listeB);   // La conversion se fait ici
-	 
-	    // Afficher maintenant le vecteur
-	    for(int i=0; i<vecR.size();i++){
-	    	System.out.println(vecR.get(i));
-	    }
-		
-		    
-		    
-		initUI(image);
 	}
+	
 
+
+	
 	private void initUI(BufferedImage image) {
 
 		setTitle("Images view");
